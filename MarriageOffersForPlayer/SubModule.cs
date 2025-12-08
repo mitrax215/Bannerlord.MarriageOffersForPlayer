999af1e46d5b55d054023d8c8a1069b4126d2ce9
MitraX <mitrax215@gmail.com>
2025-12-08 17:56:44 +0300

    Initial commit

---
+++ MarriageOffersForPlayer/SubModule.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+
+
+namespace MarriageOffersForPlayer
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+            Harmony harmony = new Harmony("com.MarriageOffersForPlayer");
+
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Marriage offers for player
